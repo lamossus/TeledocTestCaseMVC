109d24690396f4441638d4b2de6807d1f2f365a4
Chetverkin Mikhail <lamossus@gmail.com>
2023-07-17 11:17:42 +0300

    started converting views to jsonresults

---
+++ TestMVC/Util/ValidationHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace TestMVC.Util
+{
+	public static class ValidationHelper
+	{
+		public static object ModelStateToJsonable(ModelStateDictionary modelState)
+		{
+			var errors = modelState.SelectMany(kvp => kvp.Value!.Errors, (kvp, e) => new {kvp.Key, e.ErrorMessage});
+
+			return new
+			{
+				isValid = modelState.IsValid,
+				PropertyErrors = errors.Where(e => !string.IsNullOrEmpty(e.Key)),
+				ModelErrors = errors.Where(e => string.IsNullOrEmpty(e.Key))
+			};
+		}
+	}
+}
